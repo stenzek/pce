67306d309e6184fba3c2629cd056e4f5067578ac
Connor McLaughlin <mclaughc@outlook.com>
2018-07-12 16:49:24 +1000

    Add incomplete UM498F chipset emulation

---
+++ src/pce/systems/pcum498f.cpp
@@ -0,0 +1,444 @@
+#include "pce/systems/pcUM498F.h"
+#include "YBaseLib/BinaryReader.h"
+#include "YBaseLib/BinaryWriter.h"
+#include "YBaseLib/ByteStream.h"
+#include "YBaseLib/Log.h"
+#include "pce/bus.h"
+#include "pce/cpu.h"
+Log_SetChannel(Systems::PCUM498F);
+
+namespace Systems {
+
+PCUM498F::PCUM498F(HostInterface* host_interface, CPU_X86::Model model /* = CPU_X86::MODEL_486 */,
+                       float cpu_frequency /* = 8000000.0f */, uint32 memory_size /* = 16 * 1024 * 1024 */)
+  : PCBase(host_interface), m_bios_file_path("romimages/um498f.awa")
+{
+  m_cpu = new CPU_X86::CPU(model, cpu_frequency);
+  m_bus = new Bus(PHYSICAL_MEMORY_BITS);
+  AllocatePhysicalMemory(memory_size, false, false);
+  AddComponents();
+}
+
+PCUM498F::~PCUM498F() {}
+
+bool PCUM498F::Initialize()
+{
+  if (!PCBase::Initialize())
+    return false;
+
+  {
+    // We have to use MMIO ROMs, because the shadowed region can only be RAM or ROM, not both.
+    // The upper binding is okay to keep as a ROM region, though, since we don't shadow it.
+    if (!AddMMIOROMFromFile(BIOS_ROM_ADDRESS, m_bios_file_path.c_str(), BIOS_ROM_SIZE) ||
+        !m_bus->CreateROMRegionFromFile(m_bios_file_path.c_str(), BIOS_ROM_MIRROR_ADDRESS, BIOS_ROM_SIZE))
+    {
+      return false;
+    }
+  }
+
+  ConnectSystemIOPorts();
+  SetCMOSVariables();
+  return true;
+}
+
+void PCUM498F::Reset()
+{
+  PCBase::Reset();
+
+  m_cmos_lock = false;
+  m_refresh_bit = false;
+
+  std::fill(m_UM498F_registers.begin(), m_UM498F_registers.end(), uint8(0));
+  m_UM498F_index_register = 0;
+  m_UM498F_unlock_stage = 2;
+
+  // Set keyboard controller input port up.
+  // b7 = Keyboard not inhibited, b5 = POST loop inactive
+  m_keyboard_controller->SetInputPort(0xA0);
+
+  // Start with A20 line on
+  SetA20State(true);
+  UpdateKeyboardControllerOutputPort();
+  UpdateShadowRAM();
+
+  // PIT #2 gate OFF, speaker OFF.
+  m_timer->SetChannelGateInput(2, false);
+  m_speaker->SetOutputEnabled(false);
+}
+
+bool PCUM498F::LoadSystemState(BinaryReader& reader)
+{
+  if (!PCBase::LoadSystemState(reader))
+    return false;
+
+  reader.SafeReadBool(&m_cmos_lock);
+  reader.SafeReadBool(&m_refresh_bit);
+  return !reader.GetErrorState();
+}
+
+bool PCUM498F::SaveSystemState(BinaryWriter& writer)
+{
+  if (!PCBase::SaveSystemState(writer))
+    return false;
+
+  writer.SafeWriteBool(m_cmos_lock);
+  writer.SafeWriteBool(m_refresh_bit);
+  return !writer.InErrorState();
+}
+
+void PCUM498F::ConnectSystemIOPorts()
+{
+  m_bus->ConnectIOPortRead(0x0028, this, std::bind(&PCUM498F::IOReadUM498FIndexRegister, this, std::placeholders::_2));
+  m_bus->ConnectIOPortWrite(0x0028, this, std::bind(&PCUM498F::IOWriteUM498FIndexRegister, this, std::placeholders::_2));
+  m_bus->ConnectIOPortRead(0x002A, this, std::bind(&PCUM498F::IOReadUM498FDataRegister, this, std::placeholders::_2));
+  m_bus->ConnectIOPortWrite(0x002A, this, std::bind(&PCUM498F::IOWriteUM498FDataRegister, this, std::placeholders::_2));
+  // System control ports
+  m_bus->ConnectIOPortRead(0x0092, this, std::bind(&PCUM498F::IOReadSystemControlPortA, this, std::placeholders::_2));
+  m_bus->ConnectIOPortWrite(0x0092, this,
+                            std::bind(&PCUM498F::IOWriteSystemControlPortA, this, std::placeholders::_2));
+  m_bus->ConnectIOPortRead(0x0061, this, std::bind(&PCUM498F::IOReadSystemControlPortB, this, std::placeholders::_2));
+  m_bus->ConnectIOPortWrite(0x0061, this,
+                            std::bind(&PCUM498F::IOWriteSystemControlPortB, this, std::placeholders::_2));
+
+  // Connect the keyboard controller output port to the lower 2 bits of system control port A.
+  m_keyboard_controller->SetOutputPortWrittenCallback([this](uint8 value, uint8 old_value, bool pulse) {
+    if (!pulse)
+      value &= ~uint8(0x01);
+    IOWriteSystemControlPortA(value & 0x03);
+    IOReadSystemControlPortA(&value);
+    m_keyboard_controller->SetOutputPort(value);
+  });
+
+  // Eat these since they're very noisy during startup..
+  m_bus->ConnectIOPortWrite(0x00E1, this, [](uint32 port, uint8 value) {});
+  m_bus->ConnectIOPortWrite(0x00E2, this, [](uint32 port, uint8 value) {});
+}
+
+void PCUM498F::IOReadUM498FIndexRegister(uint8* value)
+{
+  if (m_UM498F_unlock_stage != 0)
+  {
+    *value = 0xFF;
+    return;
+  }
+
+  *value = m_UM498F_index_register;
+}
+
+void PCUM498F::IOWriteUM498FIndexRegister(uint8 value)
+{
+  if (m_UM498F_unlock_stage == 0)
+  {
+    m_UM498F_index_register = value;
+    return;
+  }
+  else if (m_UM498F_unlock_stage == 1)
+  {
+    if (value == 0x05)
+      m_UM498F_unlock_stage = 0;
+    else
+      m_UM498F_unlock_stage = 2;
+  }
+  else
+  {
+    if (value == 0xA0)
+      m_UM498F_unlock_stage = 1;
+    else
+      m_UM498F_unlock_stage = 2;
+  }
+}
+
+void PCUM498F::IOReadUM498FDataRegister(uint8* value)
+{
+  if (m_UM498F_unlock_stage != 0)
+  {
+    *value = 0xFF;
+    return;
+  }
+
+  Log_WarningPrintf("UM498F Register Read 0x%02X -> 0x%02X", m_UM498F_index_register, m_UM498F_registers[m_UM498F_index_register]);
+  *value = m_UM498F_registers[m_UM498F_index_register];
+}
+
+void PCUM498F::IOWriteUM498FDataRegister(uint8 value)
+{
+  if (m_UM498F_unlock_stage != 0)
+    return;
+
+  Log_WarningPrintf("UM498F Register Write 0x%02X <- 0x%02X", m_UM498F_index_register, value);
+  m_UM498F_registers[m_UM498F_index_register] = value;
+
+//   if (m_UM498F_index_register == 0x13 || m_UM498F_index_register == 0x14)
+//   {
+//     // Recalculate shadowing.
+//     UpdateShadowRAM();
+//   }
+}
+
+void PCUM498F::UpdateShadowRAM()
+{
+  for (uint32 i = 0; i < 8; i++)
+  {
+    uint32 base = UINT32_C(0xC0000) + (i << 15);
+    if (m_UM498F_registers[0x13] & (1 << i))
+    {
+      // Shadowing enabled for this region.
+      const uint32 flag = m_UM498F_registers[0x14] & 0x03;
+      const bool readable_memory = !!(flag & 1);
+      const bool writable_memory = !!(flag & 2);
+      Log_DevPrintf("Shadowing ENABLED for 0x%08X-0x%08X (type %u, readable=%s, writable=%s)", base, base + SHADOW_REGION_SIZE - 1, flag, readable_memory ? "yes" : "no", writable_memory ? "yes" : "no");
+      m_bus->SetPagesMemoryState(base, SHADOW_REGION_SIZE, readable_memory, writable_memory);
+    }
+    else
+    {
+      Log_DevPrintf("Shadowing DISABLED for 0x%08X-0x%08X", base, base + SHADOW_REGION_SIZE - 1);
+      m_bus->SetPagesMemoryState(base, SHADOW_REGION_SIZE, false, false);
+    }
+  }
+}
+
+void PCUM498F::IOReadSystemControlPortA(uint8* value)
+{
+  *value = (BoolToUInt8(m_cmos_lock) << 3) | (BoolToUInt8(GetA20State()) << 1);
+}
+
+void PCUM498F::IOWriteSystemControlPortA(uint8 value)
+{
+  Log_DevPrintf("Write system control port A: 0x%02X", ZeroExtend32(value));
+
+  // b7-6 - Activity Lights
+  // b5 - Reserved
+  // b4 - Watchdog Timeout
+  // b3 - CMOS Security Lock
+  // b2 - Reserved
+  // b1 - A20 Active
+  // b0 - System Reset
+
+  bool cmos_security_lock = !!(value & (1 << 2));
+  bool new_a20_state = !!(value & (1 << 1));
+  bool system_reset = !!(value & (1 << 0));
+
+  // Update A20 state
+  if (GetA20State() != new_a20_state)
+  {
+    SetA20State(new_a20_state);
+    UpdateKeyboardControllerOutputPort();
+  }
+
+  m_cmos_lock = cmos_security_lock;
+
+  // System reset?
+  // We do this last as it's going to destroy everything.
+  // TODO: We should probably put it on an event though..
+  if (system_reset)
+  {
+    Log_WarningPrintf("CPU reset via system control port");
+    m_cpu->Reset();
+  }
+}
+
+void PCUM498F::IOReadSystemControlPortB(uint8* value)
+{
+  *value = (BoolToUInt8(m_timer->GetChannelGateInput(2)) << 0) |  // Timer 2 gate input
+           (BoolToUInt8(m_speaker->IsOutputEnabled()) << 1) |     // Speaker data status
+           (BoolToUInt8(m_refresh_bit) << 4) |                    // Triggers with each memory refresh
+           (BoolToUInt8(m_timer->GetChannelOutputState(2)) << 5); // Raw timer 2 output
+
+  // Seems that we can get away with faking this every read.
+  // The refresh controller steps one refresh address every 15 microseconds. Each refresh cycle
+  // requires eight clock cycles to refresh all of the system's dynamic memory; 256 refresh cycles
+  // are required every 4 milliseconds, but the system hardware refreshes every 3.84ms.
+  m_refresh_bit ^= true;
+}
+
+void PCUM498F::IOWriteSystemControlPortB(uint8 value)
+{
+  Log_DevPrintf("Write system control port B: 0x%02X", ZeroExtend32(value));
+
+  m_timer->SetChannelGateInput(2, !!(value & (1 << 0))); // Timer 2 gate input
+  m_speaker->SetOutputEnabled(!!(value & (1 << 1)));     // Speaker data enable
+}
+
+void PCUM498F::UpdateKeyboardControllerOutputPort()
+{
+  uint8 value = m_keyboard_controller->GetOutputPort();
+  value &= ~uint8(0x03);
+  value |= (BoolToUInt8(GetA20State()) << 1);
+  m_keyboard_controller->SetOutputPort(value);
+}
+
+void PCUM498F::AddComponents()
+{
+  m_keyboard_controller = new HW::i8042_PS2();
+  m_dma_controller = new HW::i8237_DMA();
+  m_timer = new HW::i8253_PIT();
+  m_interrupt_controller = new HW::i8259_PIC();
+  m_cmos = new HW::CMOS();
+
+  AddComponent(m_interrupt_controller);
+  AddComponent(m_dma_controller);
+  AddComponent(m_timer);
+  AddComponent(m_keyboard_controller);
+  AddComponent(m_cmos);
+
+  m_fdd_controller = new HW::FDC(m_dma_controller);
+  m_hdd_controller = new HW::HDC(HW::HDC::CHANNEL_PRIMARY);
+
+  AddComponent(m_fdd_controller);
+  AddComponent(m_hdd_controller);
+
+  // Connect channel 0 of the PIT to the interrupt controller
+  m_timer->SetChannelOutputChangeCallback(0,
+                                          [this](bool value) { m_interrupt_controller->SetInterruptState(0, value); });
+
+  m_speaker = new HW::PCSpeaker();
+  AddComponent(m_speaker);
+
+  // Connect channel 2 of the PIT to the speaker
+  m_timer->SetChannelOutputChangeCallback(2, [this](bool value) { m_speaker->SetLevel(value); });
+}
+
+void PCUM498F::SetCMOSVariables()
+{
+  static const uint8 cmos_defaults[][2] = {
+    {0x0E, 0x00}, {0x0F, 0x00}, {0x10, 0x24}, {0x11, 0x3B}, {0x12, 0xF0}, {0x13, 0x30}, {0x14, 0x4D}, {0x15, 0x80},
+    {0x16, 0x02}, {0x17, 0x80}, {0x18, 0x0D}, {0x19, 0x2F}, {0x1A, 0x00}, {0x1B, 0x7B}, {0x1C, 0x00}, {0x1D, 0x2D},
+    {0x1E, 0x06}, {0x1F, 0x00}, {0x20, 0x08}, {0x21, 0x07}, {0x22, 0x00}, {0x23, 0x08}, {0x24, 0x00}, {0x25, 0x00},
+    {0x26, 0x00}, {0x27, 0x00}, {0x28, 0x00}, {0x29, 0x00}, {0x2A, 0x00}, {0x2B, 0x00}, {0x2C, 0x00}, {0x2D, 0x7A},
+    {0x2E, 0x04}, {0x2F, 0x49}, {0x30, 0x80}, {0x31, 0x0D}, {0x34, 0x00}, {0x35, 0x0F}, {0x36, 0x20}, {0x37, 0x80},
+    {0x38, 0x1B}, {0x39, 0x7B}, {0x3A, 0x21}, {0x3B, 0x00}, {0x3C, 0x00}, {0x3D, 0x00}, {0x3E, 0x0D}, {0x3F, 0x0B},
+    {0x40, 0x00}, {0x41, 0x82}, {0x42, 0x00}, {0x43, 0x00}, {0x44, 0x00}, {0x45, 0x05}, {0x46, 0x00}, {0x47, 0x04},
+    {0x48, 0x02}, {0x49, 0x01}, {0x4A, 0x00}, {0x4B, 0x00}, {0x4C, 0x00}, {0x4D, 0xFF}, {0x4E, 0xFF}, {0x4F, 0xFF},
+    {0x50, 0xC0}, {0x51, 0x9C}, {0x52, 0xD0}, {0x53, 0x98}, {0x54, 0xD0}, {0x55, 0x98}, {0x56, 0xF0}, {0x57, 0x98},
+    {0x58, 0x00}, {0x59, 0x04}, {0x5A, 0xF7}, {0x5B, 0xF7}, {0x5C, 0x00}, {0x5D, 0x00}, {0x5E, 0x00}, {0x5F, 0x00},
+    {0x60, 0x00}, {0x61, 0x10}, {0x62, 0x00}, {0x63, 0x00}, {0x64, 0x00}, {0x65, 0x00}, {0x66, 0x00}, {0x67, 0x00},
+    {0x68, 0x00}, {0x69, 0x00}, {0x6A, 0x01}, {0x6B, 0x40}, {0x6C, 0x00}, {0x6D, 0x00}, {0x6E, 0x00}, {0x6F, 0x00},
+    {0x70, 0x0A}, {0x71, 0x06}, {0x72, 0x0C}, {0x73, 0x07}, {0x74, 0x00}, {0x75, 0x00}, {0x76, 0x00}, {0x77, 0x00},
+    {0x78, 0x00}, {0x79, 0x00}};
+
+  for (size_t i = 0; i < countof(cmos_defaults); i++)
+    m_cmos->SetVariable(cmos_defaults[i][0], cmos_defaults[i][1]);
+
+  PhysicalMemoryAddress base_memory_in_k = GetBaseMemorySize() / 1024;
+  m_cmos->SetWordVariable(0x15, Truncate16(base_memory_in_k));
+  Log_DevPrintf("Base memory in KB: %u", Truncate32(base_memory_in_k));
+
+  PhysicalMemoryAddress extended_memory_in_k = GetExtendedMemorySize() / 1024;
+  Log_DevPrintf("Extended memory in KB: %u", Truncate32(extended_memory_in_k));
+
+  uint32 fdd_drive_type_variable = 0;
+  for (uint32 i = 0; i < HW::FDC::MAX_DRIVES; i++)
+  {
+    HW::FDC::DriveType drive_type = m_fdd_controller->GetDriveType_(i);
+    HW::FDC::DiskType disk_type = m_fdd_controller->GetDiskType(i);
+    if (drive_type == HW::FDC::DriveType_None)
+      continue;
+
+    // 1 - 360K 5.25", 2 - 1.2MB 5.25", 3 - 720K 3.5", 4 - 1.44MB 3.5", 5 - 2.88M 3.5"
+    uint32 cmos_type;
+    switch (drive_type)
+    {
+      case HW::FDC::DriveType_5_25:
+      {
+        switch (disk_type)
+        {
+          case HW::FDC::DiskType_160K:
+          case HW::FDC::DiskType_180K:
+          case HW::FDC::DiskType_320K:
+          case HW::FDC::DiskType_360K:
+          case HW::FDC::DiskType_640K:
+          case HW::FDC::DiskType_1220K:
+            cmos_type = 2;
+            break;
+          default:
+            cmos_type = 0;
+            break;
+        }
+      }
+      break;
+
+      case HW::FDC::DriveType_3_5:
+      {
+        switch (disk_type)
+        {
+          case HW::FDC::DiskType_720K:
+          case HW::FDC::DiskType_1440K:
+            cmos_type = 4;
+            break;
+          case HW::FDC::DiskType_2880K:
+            cmos_type = 5;
+            break;
+          default:
+            cmos_type = 0;
+            break;
+        }
+      }
+      break;
+
+      default:
+        cmos_type = 0;
+        break;
+    }
+
+    if (i == 0)
+      fdd_drive_type_variable |= ((cmos_type & 0xF) << 4);
+    else if (i == 1)
+      fdd_drive_type_variable |= ((cmos_type & 0xF) << 0);
+  }
+  m_cmos->SetVariable(0x10, Truncate8(fdd_drive_type_variable));
+
+  // Equipment byte
+  uint8 equipment_byte = 0;
+  // equipment_byte |= (1 << 1);     // coprocessor installed
+  if (m_fdd_controller->GetDriveCount() > 0)
+  {
+    equipment_byte |= (1 << 0); // disk available for boot
+    if (m_fdd_controller->GetDriveCount() > 1)
+      equipment_byte |= (0b01 << 7); // 2 drives installed
+  }
+  m_cmos->SetVariable(0x14, equipment_byte);
+
+  // Boot from HDD first
+  // Legacy - 0 - C: -> A:, 1 - A: -> C:
+  m_cmos->SetVariable(0x2D, (0 << 5));
+
+  // HDD information
+  m_cmos->SetVariable(0x12, 0);
+  if (m_hdd_controller->IsDrivePresent(0))
+  {
+    m_cmos->SetVariable(0x12, m_cmos->GetVariable(0x12) | 0xF0);
+    m_cmos->SetVariable(0x19, 47); // user-defined type
+    m_cmos->SetVariable(0x1B, Truncate8(m_hdd_controller->GetDriveCylinders(0)));
+    m_cmos->SetVariable(0x1C, Truncate8(m_hdd_controller->GetDriveCylinders(0) >> 8));
+    m_cmos->SetVariable(0x1D, Truncate8(m_hdd_controller->GetDriveHeads(0)));
+    m_cmos->SetVariable(0x1E, 0xFF);
+    m_cmos->SetVariable(0x1F, 0xFF);
+    m_cmos->SetVariable(0x20, 0xC0 | ((m_hdd_controller->GetDriveHeads(0) > 8) ? 8 : 0));
+    m_cmos->SetVariable(0x21, m_cmos->GetVariable(0x1B));
+    m_cmos->SetVariable(0x22, m_cmos->GetVariable(0x1C));
+    m_cmos->SetVariable(0x23, Truncate8(m_hdd_controller->GetDriveSectors(0)));
+  }
+  if (m_hdd_controller->IsDrivePresent(1))
+  {
+    m_cmos->SetVariable(0x12, m_cmos->GetVariable(0x12) | 0x0F);
+    m_cmos->SetVariable(0x1A, 48); // user-defined type
+    m_cmos->SetVariable(0x24, Truncate8(m_hdd_controller->GetDriveCylinders(1)));
+    m_cmos->SetVariable(0x25, Truncate8(m_hdd_controller->GetDriveCylinders(1) >> 8));
+    m_cmos->SetVariable(0x26, Truncate8(m_hdd_controller->GetDriveHeads(1)));
+    m_cmos->SetVariable(0x27, 0xFF);
+    m_cmos->SetVariable(0x28, 0xFF);
+    m_cmos->SetVariable(0x29, 0xC0 | ((m_hdd_controller->GetDriveHeads(1) > 8) ? 8 : 0));
+    m_cmos->SetVariable(0x2A, m_cmos->GetVariable(0x1B));
+    m_cmos->SetVariable(0x2B, m_cmos->GetVariable(0x1C));
+    m_cmos->SetVariable(0x2C, Truncate8(m_hdd_controller->GetDriveSectors(1)));
+  }
+
+  // Adjust CMOS checksum over 10-2D
+  uint16 checksum = 0;
+  for (uint8 i = 0x10; i <= 0x2D; i++)
+    checksum += ZeroExtend16(m_cmos->GetVariable(i));
+  m_cmos->SetVariable(0x2E, Truncate8(checksum >> 8));
+  m_cmos->SetVariable(0x2F, Truncate8(checksum));
+}
+
+} // namespace Systems
---
+++ src/pce/systems/pcum498f.h
@@ -0,0 +1,89 @@
+#pragma once
+#include "pce/bitfield.h"
+#include "pce/cpu_x86/cpu.h"
+#include "pce/hw/cmos.h"
+#include "pce/hw/fdc.h"
+#include "pce/hw/hdc.h"
+#include "pce/hw/i8237_dma.h"
+#include "pce/hw/i8253_pit.h"
+#include "pce/hw/i8259_pic.h"
+#include "pce/hw/pcspeaker.h"
+#include "pce/hw/i8042_ps2.h"
+#include "pce/systems/pcbase.h"
+
+class ByteStream;
+
+namespace Systems {
+
+class PCUM498F : public PCBase
+{
+public:
+  PCUM498F(HostInterface* host_interface, CPU_X86::Model model = CPU_X86::MODEL_486, float cpu_frequency = 2000000.0f,
+             uint32 memory_size = 16 * 1024 * 1024);
+  ~PCUM498F();
+
+  const char* GetSystemName() const override { return "AMI 486"; }
+  InterruptController* GetInterruptController() const override { return m_interrupt_controller; }
+  bool Initialize() override;
+  void Reset() override;
+
+  void SetBIOSFilePath(const std::string& path) { m_bios_file_path = path; }
+
+  auto GetFDDController() const { return m_fdd_controller; }
+  auto GetHDDController() const { return m_hdd_controller; }
+  auto GetKeyboardController() const { return m_keyboard_controller; }
+  auto GetDMAController() const { return m_dma_controller; }
+  auto GetTimer() const { return m_timer; }
+  auto GetCMOS() const { return m_cmos; }
+
+private:
+  static const uint32 PHYSICAL_MEMORY_BITS = 32;
+  static const PhysicalMemoryAddress BIOS_ROM_ADDRESS = 0xF0000;
+  static const PhysicalMemoryAddress BIOS_ROM_MIRROR_ADDRESS = 0xFFFF0000;
+  static const uint32 BIOS_ROM_SIZE = 65536;
+  static const uint32 SHADOW_REGION_BASE = 0xC0000;
+  static const uint32 SHADOW_REGION_SIZE = 0x8000;
+  static const uint32 SHADOW_REGION_COUNT = 8;
+
+  virtual bool LoadSystemState(BinaryReader& reader) override;
+  virtual bool SaveSystemState(BinaryWriter& writer) override;
+
+  void ConnectSystemIOPorts();
+  void AddComponents();
+  void SetCMOSVariables();
+  void UpdateShadowRAM();
+
+  void IOReadUM498FIndexRegister(uint8* value);
+  void IOWriteUM498FIndexRegister(uint8 value);
+  void IOReadUM498FDataRegister(uint8* value);
+  void IOWriteUM498FDataRegister(uint8 value);
+
+  void IOReadSystemControlPortA(uint8* value);
+  void IOWriteSystemControlPortA(uint8 value);
+  void IOReadSystemControlPortB(uint8* value);
+  void IOWriteSystemControlPortB(uint8 value);
+  void UpdateKeyboardControllerOutputPort();
+
+  std::string m_bios_file_path;
+
+  HW::i8042_PS2* m_keyboard_controller = nullptr;
+  HW::i8237_DMA* m_dma_controller = nullptr;
+  HW::i8253_PIT* m_timer = nullptr;
+  HW::i8259_PIC* m_interrupt_controller = nullptr;
+
+  HW::CMOS* m_cmos = nullptr;
+
+  HW::PCSpeaker* m_speaker = nullptr;
+
+  HW::FDC* m_fdd_controller = nullptr;
+  HW::HDC* m_hdd_controller = nullptr;
+
+  std::array<uint8, 256> m_UM498F_registers{};
+  uint8 m_UM498F_index_register = 0;
+  uint8 m_UM498F_unlock_stage = 2;
+
+  bool m_cmos_lock = false;
+  bool m_refresh_bit = false;
+};
+
+} // namespace Systems
