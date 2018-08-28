f351751fcb4624b3dd8c4efb25509dbaafda6b8b
Connor McLaughlin <mclaughc@outlook.com>
2018-08-28 23:49:00 +1000

    Introduce new HDDImage class - logging/replayable disk images

--- src/pce/types.h
+++ src/pce/types.h
@@ -4,6 +4,15 @@
 #include <cstring>
 #include <type_traits>
 
+using s8 = int8_t;
+using u8 = uint8_t;
+using s16 = int16_t;
+using u16 = uint16_t;
+using s32 = int32_t;
+using u32 = uint32_t;
+using s64 = int64_t;
+using u64 = uint64_t;
+
 // Physical memory addresses are 32-bits wide
 using PhysicalMemoryAddress = uint32;
 using LinearMemoryAddress = uint32;
