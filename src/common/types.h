1b107b5a0b24b0a80708d2daefd101789f290b71
Connor McLaughlin <mclaughc@outlook.com>
2018-09-25 18:03:49 +1000

    common: Make flag operators constexpr

--- src/common/types.h
+++ src/common/types.h
@@ -136,38 +136,38 @@ constexpr bool ConvertToBoolUnchecked(TValue value)
 
 // Enum class bitwise operators
 #define IMPLEMENT_ENUM_CLASS_BITWISE_OPERATORS(type_)                                                                  \
-  inline type_ operator&(type_ lhs, type_ rhs)                                                                         \
+  inline constexpr type_ operator&(type_ lhs, type_ rhs)                                                               \
   {                                                                                                                    \
     return static_cast<type_>(static_cast<std::underlying_type<type_>::type>(lhs) &                                    \
                               static_cast<std::underlying_type<type_>::type>(rhs));                                    \
   }                                                                                                                    \
-  inline type_ operator|(type_ lhs, type_ rhs)                                                                         \
+  inline constexpr type_ operator|(type_ lhs, type_ rhs)                                                               \
   {                                                                                                                    \
     return static_cast<type_>(static_cast<std::underlying_type<type_>::type>(lhs) |                                    \
                               static_cast<std::underlying_type<type_>::type>(rhs));                                    \
   }                                                                                                                    \
-  inline type_ operator^(type_ lhs, type_ rhs)                                                                         \
+  inline constexpr type_ operator^(type_ lhs, type_ rhs)                                                               \
   {                                                                                                                    \
     return static_cast<type_>(static_cast<std::underlying_type<type_>::type>(lhs) ^                                    \
                               static_cast<std::underlying_type<type_>::type>(rhs));                                    \
   }                                                                                                                    \
-  inline type_ operator~(type_ val)                                                                                    \
+  inline constexpr type_ operator~(type_ val)                                                                          \
   {                                                                                                                    \
     return static_cast<type_>(~static_cast<std::underlying_type<type_>::type>(val));                                   \
   }                                                                                                                    \
-  inline type_& operator&=(type_& lhs, type_ rhs)                                                                      \
+  inline constexpr type_& operator&=(type_& lhs, type_ rhs)                                                            \
   {                                                                                                                    \
     lhs = static_cast<type_>(static_cast<std::underlying_type<type_>::type>(lhs) &                                     \
                              static_cast<std::underlying_type<type_>::type>(rhs));                                     \
     return lhs;                                                                                                        \
   }                                                                                                                    \
-  inline type_& operator|=(type_& lhs, type_ rhs)                                                                      \
+  inline constexpr type_& operator|=(type_& lhs, type_ rhs)                                                            \
   {                                                                                                                    \
     lhs = static_cast<type_>(static_cast<std::underlying_type<type_>::type>(lhs) |                                     \
                              static_cast<std::underlying_type<type_>::type>(rhs));                                     \
     return lhs;                                                                                                        \
   }                                                                                                                    \
-  inline type_& operator^=(type_& lhs, type_ rhs)                                                                      \
+  inline constexpr type_& operator^=(type_& lhs, type_ rhs)                                                            \
   {                                                                                                                    \
     lhs = static_cast<type_>(static_cast<std::underlying_type<type_>::type>(lhs) ^                                     \
                              static_cast<std::underlying_type<type_>::type>(rhs));                                     \
